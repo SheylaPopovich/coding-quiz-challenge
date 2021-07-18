027c2d900aa54da8145fa40aeaca0d624c2389ce
Sheyla Popovich <Sheylapopovich@gmail.com>
2021-07-17 18:22:57 -0600

    Created additional HTML and CSS files

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("lets do the dang thing!")
+//console.log("lets do the dang thing!")
+var generateBtn = document.querySelector("#generate");
+
+
+
+function(){
+    console.log("You clicked it and it worked!!")
+} 
+  
+
+
+generateBtn.addEventListener("click", generateBtn);
