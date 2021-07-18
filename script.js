ef6c5b9b2093902cf7d0add7df2fc3aab0203c98
Sheyla Popovich <Sheylapopovich@gmail.com>
2021-07-17 23:19:51 -0600

    created a timer for quiz

--- script.js
+++ script.js
@@ -1,12 +1,53 @@
-//console.log("lets do the dang thing!")
-var generateBtn = document.querySelector("#generate");
 
 
 
-function(){
-    console.log("You clicked it and it worked!!")
-} 
-  
 
 
-generateBtn.addEventListener("click", generateBtn);
+/*const questions = [
+    {
+        question: "What is 2+2?",
+        choice1: "2",
+        choice1: "4",
+        choice1: "16",
+        choice1: "33",
+        answer: 2,
+    },
+]*/
+
+
+//create addeventlistener for start game btn
+/*let startBtn = document.getElementById("#startBtn");
+
+startBtn.addEventListener("click", startGame)
+
+
+function startGame() {
+    console.log("lets do the dang thing!")
+}*/
+
+var timerEl = document.querySelector(".timer");
+
+var secondsLeft = 80;
+
+function setTime() {
+    var timerInterval = setInterval(function() {
+        secondsLeft--;
+        timerEl.textContent = "Timer: " + secondsLeft; 
+
+        if(secondsLeft === 0) {
+            clearInterval(timerInterval);
+            sendMessage();
+        } 
+
+    }, 80);
+}
+
+
+
+
+function sendMessage(){
+    timerEl.textContent= "You lost";
+
+}
+
+setTime();
