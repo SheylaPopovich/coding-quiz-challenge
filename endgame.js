7580978e2bf9d8cd4c2afb344f3062683e8981ea
Sheyla Popovich <Sheylapopovich@gmail.com>
2021-07-20 00:31:38 -0600

    created a highscore.html page to showcase top scores

--- endgame.js
+++ endgame.js
@@ -2,15 +2,36 @@ const username = document.getElementById("username");
 const saveScoreBtn = document.getElementById("saveScoreBtn");
 const finalScore = document.getElementById("finalScore");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
+
+//covert array to string using JSON.stringify
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+const TOP_THREE_SCORES = 3;
+
+
 finalScore.innerText = mostRecentScore;
 
 
-username.addEventListener("keyup", () =>{
-    console.log(username.value);
-    saveScoreBtn.disabled = !username.value;
-})
+
+
+//disabled saveScoreBtn to direct user to input initials in placeholder
+username.addEventListener("keyup", () => {
+   saveScoreBtn.disabled = !username.value;
+});
 
 saveHighScore = e => {
-    console.log ("clicked the save button!");
     e.preventDefault();
+ 
+    const score = {
+        score: Math.floor(Math.random() * 100),
+        name: username.value 
+    };
+
+    highScores.push(score);
+
+    highScores.sort((a, b) => b.score - a.score);
+
+    highScores.splice(3);
+
+    console.log(highScores);
 };
+
